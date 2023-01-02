7a2f73730d18cdca5978672012805685d996e3e0
Tomer Klein <tomer.klein@gmail.com>
2023-01-02 11:45:30 +0200

    Added FastAPI framework to the application #1

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from adb_shell.adb_device import AdbDeviceTcp, AdbDeviceUsb
-from adb_shell.auth.sign_pythonrsa import PythonRSASigner
-from adb_shell.auth.keygen import keygen
 import time
+import uvicorn
 from os import path
+from loguru import logger
+from fastapi import FastAPI, Request
+from adb_shell.auth.keygen import keygen
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from adb_shell.auth.sign_pythonrsa import PythonRSASigner
+from adb_shell.adb_device import AdbDeviceTcp, AdbDeviceUsb
+
+
+
 
 
 KEYS_PATH = './keys/adb'
@@ -27,3 +36,20 @@ def load_keys():
         pub = f.read()
     signer = PythonRSASigner(pub, priv)
 
+
+
+app = FastAPI(title="Virtual Remote for android tv", description="Virtualy control you android tv devices", version="1.0.0",  contact={"name": "Tomer Klein", "email": "tomer.klein@gmail.com", "url": "https://github.com/t0mer/virtual-remote"})
+logger.info("Configuring app")
+app.mount("/dist", StaticFiles(directory="dist"), name="dist")
+templates = Jinja2Templates(directory="templates/")
+
+
+@app.get('/remotes/mibox', include_in_schema=False)
+def index(request: Request):
+    return templates.TemplateResponse('mibox.html', context={'request': request})
+
+
+if __name__ == "__main__":
+    load_keys()
+    logger.info("Virtual remote is up and running")
+    uvicorn.run(app, host="0.0.0.0", port=80)
